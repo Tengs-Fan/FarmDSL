cda0f45dde980c61e609fde824b8dfecf9e7c6f5
Tengs Penkwe <tengspenkwe@gmail.com>
2024-01-23 13:13:40 -0800

    start working

--- src/Parse.ts
+++ src/Parse.ts
@@ -1,1 +1,15 @@
-import * as antlr from 'antlr4ts';
+import * as antlr from 'antlr4';
+import FarmExprLexer from '../lang/FarmExprLexer';
+import FarmExprParser from '../lang/FarmExprParser';
+
+function parseExpression(input: string) {
+    const chars = antlr.CharStreams.fromString(input);
+    const lexer = new FarmExprLexer(chars);
+    const tokens = new antlr.CommonTokenStream(lexer); 
+    const parser = new FarmExprParser(tokens);
+
+    const tree = parser.expr();
+    console.log(tree.toStringTree(parser.ruleNames, parser));
+}
+
+parseExpression('1 + 2 * 3');
