95ae26b6ea369d2c203b6965cdaccd9647df4eb9
Tengs Penkwe <tengspenkwe@gmail.com>
2024-02-01 03:51:45 -0800

    add FunctionError type

--- src/Error.ts
+++ src/Error.ts
@@ -1,8 +1,13 @@
-
 export class ExprError extends Error {
     constructor(message: string) {
         super(message);
     }
+}
+
+export class FunctionError extends Error {
+  constructor(message: string) {
+    super(message);
+  }
 }
 
 export class VariableError extends Error {
