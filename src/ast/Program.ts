307bdd0107067bb74bb3e8552821893bcb6c0152
Tengs Penkwe <tengspenkwe@gmail.com>
2024-02-15 15:00:08 -0800

    remove self-defined function

--- src/ast/Program.ts
+++ src/ast/Program.ts
@@ -1,16 +1,13 @@
 import {Result} from "./Type";
 import {Context} from "../vm/Context";
 import {ASTNode} from "./Ast";
-import {Statement, ReturnStatement} from "./Statement";
-import {Func} from "./Func";
+import {Statement} from "./Statement";
 
 export class Program implements ASTNode {
     stmts: Statement[];
-    funcs: Func[];
 
     constructor() {
         this.stmts = [];
-        this.funcs = [];
     }
     addStatement(stmt: Statement) {
         this.stmts.push(stmt);
@@ -18,25 +15,16 @@ export class Program implements ASTNode {
     addStatements(stmts: Statement[]) {
         this.stmts.push(...stmts);
     }
-    addFunction(func: Func) {
-        this.funcs.push(func);
-    }
 
     eval(ctx: Context): Result {
         // Register all functions
         // TODO: this would be ok since the root program is only evaluated once,
         // but if we have a program that can be evaluated multiple times, it will
         // thow an error since the function is already registered
-        this.funcs.forEach((func) => {
-            ctx.newFunction(func.name, func);
-        });
 
         let lastResult = new Result("Null", null);
 
         this.stmts.forEach((stmt) => {
-            if (stmt instanceof ReturnStatement) {
-                return stmt.eval(ctx);
-            }
             lastResult = stmt.eval(ctx);
         });
 
