3d836aae9a3681770cf3431dd79cf4d184530097
Tengs Penkwe <tengspenkwe@gmail.com>
2024-03-10 15:59:55 -0700

    basic settings of gradle

---
+++ app/src/main/java/org/example/App.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import java.util.*;
+
+public class App {
+    public String getGreeting() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new App().getGreeting());
+    }
+}
